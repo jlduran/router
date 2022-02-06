b80b7366bf2190bc89ca417854cd30a8b09388e3
Jose Luis Duran <jlduran@gmail.com>
2022-02-06 07:08:53 -0500

    Reactivate old method

--- .cirrus/preheat_poudriere.sh
+++ .cirrus/preheat_poudriere.sh
@@ -4,23 +4,22 @@
 #
 # Download built packages to avoid timeouts.
 
-#fetch http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/packagesite.txz
-#tar -zxf packagesite.txz
+fetch http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/packagesite.txz
+tar -zxf packagesite.txz
 
 mkdir -p /usr/local/poudriere/data/packages/router-quarterly/All
 
-#for pkg in $(cat $CIRRUS_WORKING_DIR/pkglist); do
-#	path=$(jq --arg origin "$pkg" --arg name "${pkg##*/}" 'select(.origin == $origin) | select(.name == $name) | .path' packagesite.yaml | tr -d '"')
-#
-#	# fetch .pkg
-#	fetch -o /usr/local/poudriere/data/packages/router-quarterly/"$(echo $path)" http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/"$(echo $path)"
-#	# fetch .txz
-#	path_txz="$(echo ${path%%.pkg}).txz"
-#	fetch -o /usr/local/poudriere/data/packages/router-quarterly/"$(echo $path_txz)" http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/"$(echo $path_txz)"
-#done
+for pkg in $(cat $CIRRUS_WORKING_DIR/pkglist); do
+	path=$(jq --arg origin "$pkg" --arg name "${pkg##*/}" 'select(.origin == $origin) | select(.name == $name) | .path' packagesite.yaml | tr -d '"')
 
-pkg fetch -dy $(cat $CIRRUS_WORKING_DIR/pkglist)
-cp /var/cache/pkg/* /usr/local/poudriere/data/packages/router-quarterly/All
+	# fetch .pkg
+	fetch -o /usr/local/poudriere/data/packages/router-quarterly/"$(echo $path)" http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/"$(echo $path)"
+	path_txz="$(echo ${path%%.pkg}).txz"
+	ln -s /usr/local/poudriere/data/packages/router-quarterly/"$(echo $path)" /usr/local/poudriere/data/packages/router-quarterly/"$(echo $path_txz)"
+done
+
+#pkg fetch -dy $(cat $CIRRUS_WORKING_DIR/pkglist)
+#cp /var/cache/pkg/* /usr/local/poudriere/data/packages/router-quarterly/All
 
 # XXX Patch poudriere
 fetch -o /usr/local/share/poudriere/image_zfs.sh https://raw.githubusercontent.com/freebsd/poudriere/master/src/share/poudriere/image_zfs.sh
