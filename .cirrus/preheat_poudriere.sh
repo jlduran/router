c5bcfd779104de89c2eb39f1cba6b70d7860835d
Jose Luis Duran <jlduran@gmail.com>
2022-02-06 06:39:00 -0500

    Different pre-heating method

--- .cirrus/preheat_poudriere.sh
+++ .cirrus/preheat_poudriere.sh
@@ -4,12 +4,20 @@
 #
 # Download built packages to avoid timeouts.
 
-fetch http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/packagesite.txz
-tar -zxf packagesite.txz
+#fetch http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/packagesite.txz
+#tar -zxf packagesite.txz
 
 mkdir -p /usr/local/poudriere/data/packages/router-quarterly/All
 
-for pkg in $(cat $CIRRUS_WORKING_DIR/pkglist); do
-	path=$(jq --arg origin "$pkg" --arg name "${pkg##*/}" 'select(.origin == $origin) | select(.name == $name) | .path' packagesite.yaml | tr -d '"')
-	fetch -o /usr/local/poudriere/data/packages/router-quarterly/"$(echo $path)" http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/"$(echo $path)"
-done
+#for pkg in $(cat $CIRRUS_WORKING_DIR/pkglist); do
+#	path=$(jq --arg origin "$pkg" --arg name "${pkg##*/}" 'select(.origin == $origin) | select(.name == $name) | .path' packagesite.yaml | tr -d '"')
+#
+#	# fetch .pkg
+#	fetch -o /usr/local/poudriere/data/packages/router-quarterly/"$(echo $path)" http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/"$(echo $path)"
+#	# fetch .txz
+#	path_txz="$(echo ${path%%.pkg}).txz"
+#	fetch -o /usr/local/poudriere/data/packages/router-quarterly/"$(echo $path_txz)" http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/"$(echo $path_txz)"
+#done
+
+pkg fetch -dy $(cat $CIRRUS_WORKING_DIR/pkglist)
+cp /var/cache/pkg/* /usr/local/poudriere/data/packages/router-quarterly/All
