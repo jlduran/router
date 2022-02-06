cd4cbb3c0b4d3e5a864a5f9b746e04012e04d6c2
Jose Luis Duran <jlduran@gmail.com>
2022-02-06 16:53:38 -0500

    Pre-heat: Fetch pkg
    Otherwise poudriere will remove everything

--- .cirrus/preheat_poudriere.sh
+++ .cirrus/preheat_poudriere.sh
@@ -1,25 +1,53 @@
 #!/bin/sh
 
-# Preheat Poudriere:
+# Pre-heat Poudriere
 #
-# Download built packages to avoid timeouts.
+# Download pre-built packages to avoid timeouts and speed up the process.
 
-fetch http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/packagesite.txz
-tar -zxf packagesite.txz
+_preheat_create_packages_directory()
+{
+	mkdir -p /usr/local/poudriere/data/packages/router-quarterly/All
+}
 
-mkdir -p /usr/local/poudriere/data/packages/router-quarterly/All
+_preheat_fetch_packagesite()
+{
+	fetch http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/packagesite.txz
+	tar -zxf packagesite.txz packagesite.yaml
+}
 
-for pkg in $(cat $CIRRUS_WORKING_DIR/pkglist); do
-	path=$(jq --arg origin "$pkg" --arg name "${pkg##*/}" 'select(.origin == $origin) | select(.name == $name) | .path' packagesite.yaml | tr -d '"')
+_preheat_fetch_pkg()
+{
+	_origin="$1"
 
-	# fetch .pkg
-	fetch -o /usr/local/poudriere/data/packages/router-quarterly/"$(echo $path)" http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/"$(echo $path)"
-	path_txz="$(echo ${path%%.pkg}).txz"
-	ln -s /usr/local/poudriere/data/packages/router-quarterly/"$(echo $path)" /usr/local/poudriere/data/packages/router-quarterly/"$(echo $path_txz)"
+	_path="$(_preheat_origin_get_path $_origin)"
+	fetch -o /usr/local/poudriere/data/packages/router-quarterly/"${_path}" http://pkg.freebsd.org/FreeBSD:13:amd64/quarterly/"${_path}"
+
+	_path_txz="$(echo ${path%%.pkg}).txz"
+	ln -s /usr/local/poudriere/data/packages/router-quarterly/"${_path}" /usr/local/poudriere/data/packages/router-quarterly/"${_path_txz}"
+}
+
+_preheat_origin_get_path()
+{
+	_origin="$1"
+
+	jq --arg origin "$_origin" --arg name "${_origin##*/}" 'select(.origin == $origin) | select(.name == $name) | .path' packagesite.yaml | tr -d '"'
+}
+
+_XXX_patch_poudriere()
+{
+	fetch -o /usr/local/share/poudriere/image_zfs.sh https://raw.githubusercontent.com/freebsd/poudriere/master/src/share/poudriere/image_zfs.sh
+}
+
+# Pre-heat
+#
+_preheat_create_packages_directory
+_XXX_patch_poudriere
+_preheat_fetch_packagesite
+
+## fetch pkg
+_preheat_fetch_pkg "ports-mgmt/pkg"
+
+## fetch each origin from pkglist
+for _origin in $(cat $CIRRUS_WORKING_DIR/pkglist); do
+	_preheat_fetch_pkg "$_origin"
 done
-
-#pkg fetch -dy $(cat $CIRRUS_WORKING_DIR/pkglist)
-#cp /var/cache/pkg/* /usr/local/poudriere/data/packages/router-quarterly/All
-
-# XXX Patch poudriere
-fetch -o /usr/local/share/poudriere/image_zfs.sh https://raw.githubusercontent.com/freebsd/poudriere/master/src/share/poudriere/image_zfs.sh
